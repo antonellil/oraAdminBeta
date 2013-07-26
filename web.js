3101fb4d8b77a13453529c5c5610842f7b0033a2
antonellil <lpa22@cornell.edu>
2013-07-26 03:28:20 -0400

    pdate

--- web.js
+++ web.js
@@ -6,17 +6,16 @@ app.use(express.logger());
 
 console.log(process.env.DATABASE_URL);
 
+app.get('/', function(request, response) {
+  console.log("Sending");
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   var query = client.query('SELECT * FROM venues');
 
   query.on('row', function(row) {
     console.log(JSON.stringify(row));
   });
-});
+	});
 
-
-app.get('/', function(request, response) {
-  console.log("Sending");
   response.send('Hello World! Let us rejoice! Twice!'+process.env.PORT);
 });
 
