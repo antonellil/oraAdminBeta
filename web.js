3a0d3ec2f315ad9de1981a3b3e0828b9b9141f00
antonellil <lpa22@cornell.edu>
2013-07-26 02:13:53 -0400

    Slight update

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Let us rejoice!');
+  response.send('Hello World! Let us rejoice! Twice!');
 });
 
 var port = process.env.PORT || 5000;
