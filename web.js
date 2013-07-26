875e2c5f42e31cbff64d5c070a1cd08d9d035e37
antonellil <lpa22@cornell.edu>
2013-07-26 02:25:16 -0400

    Added port

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Let us rejoice! Twice!');
+  response.send('Hello World! Let us rejoice! Twice!'+process.env.PORT);
 });
 
 var port = process.env.PORT || 5000;
