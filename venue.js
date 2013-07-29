73384915857f0782025db877217c2490477f8e19
antonellil <lpa22@cornell.edu>
2013-07-28 22:14:11 -0400

    Added files

---
+++ venue.js
@@ -0,0 +1,35 @@
+var pg = require('pg');
+var util = require('util');
+
+var conString = process.env.DATABASE_URL || "postgres://soccerswim8:aaaaaaa1@localhost/mydb";
+
+var client = new pg.Client(conString);
+
+client.connect();
+ 
+exports.getAll = function(req, res) {
+    var  query = client.query("select * from venues",function(err,result){
+        if(err) {
+            res.send("Error retrieving venues");
+        } else{
+            res.send(JSON.stringify(result.rows));
+        }   
+    });
+};
+ 
+exports.addVenue = function(req, res) {
+    var venue = req.query;
+    console.log('Inserting Venue');
+    console.log(util.inspect(req.query, false, null));
+  
+    client.query("INSERT INTO venues (name) VALUES($1)",[venue.name]);
+
+    res.send('done');
+}
+ 
+exports.deleteVenue = function(req, res) {
+    var venue = req.query;
+    console.log('Deleting Venue');
+    client.query("DELETE FROM venues WHERE id = $1",[venue.id]);
+    res.send('done');
+}
