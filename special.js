6fcf7d017dfeb6e2e31742526d97324b67d7ad63
antonellil <lpa22@cornell.edu>
2013-08-17 23:26:54 -0500

    fixing geocoding saerch

--- special.js
+++ special.js
@@ -89,7 +89,7 @@ exports.getAll = function(req, res) {
                 var lowerLng = String(parseFloat(gmLng)-radius);
                 whereClause += ' and lng < '+upperLng+' and lng > '+lowerLng;
 
-                var  query = client.query("select * from specials "+whereClause+" order by point(lng,lat) <@> point("+query_params['lng']+","+query_params['lat']+") limit "+query_params['limit']+" offset "+query_params['offset'],[],function(err,result){
+                var  query = client.query("select * from specials "+whereClause+" order by point(lng,lat) <@> point("+gmLng+","+gmLat+") limit "+query_params['limit']+" offset "+query_params['offset'],[],function(err,result){
                     if(err) {
                         res.jsonp({error: true, errorMessage: String(err)});
                     } else{
